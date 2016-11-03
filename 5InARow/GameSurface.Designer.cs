ce261cc33bffb8c00a8c63b4e879c7e50ecd2c2e
Alex Ghiondea <alex.ghiondea@gmail.com>
2016-11-02 22:55:45 -0700

    Initial commit

---
+++ 5InARow/GameSurface.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _5InARow
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GameSurface = new System.Windows.Forms.Panel();
+            this.lblNext = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GameSurface
+            // 
+            this.GameSurface.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GameSurface.Location = new System.Drawing.Point(2, 44);
+            this.GameSurface.Name = "GameSurface";
+            this.GameSurface.Size = new System.Drawing.Size(541, 356);
+            this.GameSurface.TabIndex = 0;
+            this.GameSurface.Paint += new System.Windows.Forms.PaintEventHandler(this.GameSurface_Paint);
+            this.GameSurface.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GameSurface_MouseDown);
+            // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNext.Location = new System.Drawing.Point(165, 9);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(30, 32);
+            this.lblNext.TabIndex = 1;
+            this.lblNext.Text = "x";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Next move:";
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(457, 12);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(75, 23);
+            this.btnUndo.TabIndex = 3;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 401);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.lblNext);
+            this.Controls.Add(this.GameSurface);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "TicTacToe";
+            this.Text = "5 in a row tic-tac-toe";
+            this.Load += new System.EventHandler(this.TicTacToe_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel GameSurface;
+        private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUndo;
+    }
+}
+
---
+++ 5InARow/GameSurface.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5InARow
+{
+    public partial class TicTacToe : Form
+    {
+        private static Pen s_gridPen = new Pen(new SolidBrush(Color.LightGray), 1);
+        private static Font s_font = new Font("Tahoma", 6, FontStyle.Regular);
+        private int gridSize = 30;
+        private TicTacToeValue s_currentPlayer;
+
+        private static Stack<VisualNode> s_moves = new Stack<VisualNode>();
+        public TicTacToeValue CurrentPlayer
+        {
+            get { return s_currentPlayer; }
+            set { s_currentPlayer = value; lblNext.Text = value.ToString(); }
+        }
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+
+            // Enable double buffering for the drawing surface
+            typeof(Panel).InvokeMember("DoubleBuffered",
+               BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+               null, GameSurface, new object[] { true });
+        }
+
+        private void DrawGrid(Size rectangle, Graphics g)
+        {
+            int w = rectangle.Width;
+            int h = rectangle.Height;
+
+            for (int i = 0; i < h; i += gridSize)
+            {
+                g.DrawLine(s_gridPen, new Point(0, i), new Point(w, i));
+                g.DrawString(((i / gridSize) + 1).ToString(), s_font, Brushes.Gray, new Point(0, i));
+            }
+
+            for (int i = 0; i < w; i += gridSize)
+            {
+                g.DrawLine(s_gridPen, new Point(i, 0), new Point(i, h));
+                if (i > 0) // only draw the top corner once.
+                    g.DrawString(((i / gridSize) + 1).ToString(), s_font, Brushes.Gray, new Point(i, 0));
+            }
+        }
+
+        private int SnapToClosest(int actual)
+        {
+            int whole = actual / gridSize;
+            int extra = (int)Math.Floor((double)(actual % gridSize) / gridSize);
+
+            return (whole + extra);
+        }
+
+        private void GameSurface_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGrid(Size, e.Graphics);
+
+            // draw all the visual nodes
+            foreach (var node in nodes.Values)
+            {
+                node.Draw(gridSize, e.Graphics);
+            }
+        }
+
+        private void GameSurface_MouseDown(object sender, MouseEventArgs e)
+        {
+            // put a visual node
+
+            int x = SnapToClosest(e.X);
+            int y = SnapToClosest(e.Y);
+
+            AddMove(x, y);
+        }
+
+        Dictionary<string, VisualNode> nodes = new Dictionary<string, VisualNode>();
+
+        private void AddMove(int x, int y)
+        {
+            string nodeKey = Constants.GetKey(x, y);
+            if (nodes.ContainsKey(nodeKey))
+            {
+                MessageBox.Show("A move already exists there!!!");
+                return;
+            }
+
+            // check to see if it is close to any other nodes, on all sides. If not, error.
+            if (!HasNeighbours(x, y) && nodes.Count > 0)
+            {
+                MessageBox.Show("A move needs to be close to an existing node!");
+                return;
+            }
+
+            var newInternalNode = new InternalNode(CurrentPlayer);
+
+            foreach (NodeLocation value in Enum.GetValues(typeof(NodeLocation)))
+            {
+                string neighborLocation = Constants.MapDirectionToComputation[value](x, y);
+                VisualNode neighbourNode;
+                if (nodes.TryGetValue(neighborLocation, out neighbourNode))
+                {
+                    Debug.WriteLine($"{neighborLocation} in direction {value}");
+
+                    // add the existing node to the new node as neighbour
+                    newInternalNode.AddNeighbour(neighbourNode.Node, value);
+
+                    // get the reverse direction
+                    NodeLocation reverseDirection = value.GetReverseDirection();
+                    neighbourNode.Node.AddNeighbour(newInternalNode, reverseDirection);
+
+                    Debug.WriteLine($"Node {Constants.GetKey(neighbourNode.X, neighbourNode.Y)} has node {Constants.GetKey(x, y)} at {reverseDirection}.");
+                }
+            }
+
+            VisualNode newNode = new VisualNode(x, y, newInternalNode);
+            s_moves.Push(newNode);
+            nodes.Add(nodeKey, newNode);
+
+            Refresh();
+
+            if (HasWon(newInternalNode))
+            {
+                MessageBox.Show($"Winner is player {CurrentPlayer}!!!");
+
+                // reset the board
+                CurrentPlayer = TicTacToeValue.x;
+                nodes.Clear();
+                s_moves.Clear();
+                Refresh();
+            }
+            else
+            {
+                ChangePlayer();
+            }
+        }
+
+        private bool HasWon(InternalNode newInternalNode)
+        {
+            // check the 4 directions.
+            int colCount = 1 + newInternalNode.CountOnDirection(NodeLocation.TopCenter, newInternalNode.Value) + newInternalNode.CountOnDirection(NodeLocation.BottomCenter, newInternalNode.Value);
+
+            int rowCount = 1 + newInternalNode.CountOnDirection(NodeLocation.Left, newInternalNode.Value) + newInternalNode.CountOnDirection(NodeLocation.Right, newInternalNode.Value);
+
+            int bigDiagCount = 1 + newInternalNode.CountOnDirection(NodeLocation.TopLeft, newInternalNode.Value) + newInternalNode.CountOnDirection(NodeLocation.BottomRight, newInternalNode.Value);
+
+            int smallDiagCount = 1 + newInternalNode.CountOnDirection(NodeLocation.TopRight, newInternalNode.Value) + newInternalNode.CountOnDirection(NodeLocation.BottomLeft, newInternalNode.Value);
+
+            return colCount >= 5 || rowCount >= 5 || bigDiagCount >= 5 || smallDiagCount >= 5;
+        }
+
+        private void RemovePreviousMove()
+        {
+            if (s_moves.Count > 0)
+            {
+                var node = s_moves.Pop();
+                // remove itself from the neighbours
+                node.Node.RemoveFromNeighbours();
+                nodes.Remove($"{Constants.GetKey(node.X, node.Y)}");
+                // reset the current move
+                ChangePlayer();
+                Refresh();
+            }
+        }
+
+        private void ChangePlayer()
+        {
+            CurrentPlayer = (TicTacToeValue)((int)(CurrentPlayer + 1) % 2);
+        }
+
+        private bool HasNeighbours(int x, int y)
+        {
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    string testKey = Constants.GetKey(x + i, y + j);
+                    Debug.WriteLine(testKey);
+                    if (nodes.ContainsKey(testKey))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            lblNext.DataBindings.Add("Text", this, nameof(CurrentPlayer), false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            RemovePreviousMove();
+        }
+    }
+
+    public static class Constants
+    {
+        public static string GetKey(int x, int y)
+        {
+            return $"{x + 1}_{y + 1}"; //adding 1 to offset the dictionary key by 1 to match the visual cues.
+        }
+
+        public const int DirectionDifference = 100;
+
+        public static readonly Dictionary<NodeLocation, Func<int, int, string>> MapDirectionToComputation = new Dictionary<NodeLocation, Func<int, int, string>>()
+        {
+            { NodeLocation.TopLeft,      (x,y)=>$"{GetKey(x - 1, y - 1)}" },
+            { NodeLocation.BottomLeft,   (x,y)=>$"{GetKey(x - 1, y + 1)}" },
+            { NodeLocation.Left,         (x,y)=>$"{GetKey(x - 1, y)}" },
+            { NodeLocation.TopRight,     (x,y)=>$"{GetKey(x + 1, y - 1)}" },
+            { NodeLocation.BottomRight,  (x,y)=>$"{GetKey(x + 1, y + 1)}" },
+            { NodeLocation.Right,        (x,y)=>$"{GetKey(x + 1, y)}" },
+            { NodeLocation.BottomCenter, (x,y)=>$"{GetKey(x, y + 1)}" },
+            { NodeLocation.TopCenter,    (x,y)=>$"{GetKey(x, y - 1)}" }
+        };
+
+        public static NodeLocation GetReverseDirection(this NodeLocation nodeLocation)
+        {
+            return (nodeLocation + ((int)nodeLocation > DirectionDifference ? -1 : 1) * DirectionDifference);
+        }
+    }
+
+    public class VisualNode
+    {
+        public int X;
+        public int Y;
+        public InternalNode Node;
+
+        public VisualNode(int x, int y, InternalNode node)
+        {
+            X = x;
+            Y = y;
+            Node = node;
+        }
+
+        public void Draw(int gridSize, Graphics g)
+        {
+            // scale the coordinates.
+            int TopX = X * gridSize;
+            int TopY = Y * gridSize;
+            //g.DrawRectangle(Pens.Black, TopX, TopY, gridSize, gridSize);
+
+            // draw the node's value
+            var stringSize = g.MeasureString($"{Node.Value}", new Font("Consolas", 30));
+
+            var left = TopX + (gridSize - stringSize.Width) / 2;
+            var top = TopY + (gridSize - stringSize.Height) / 2;
+
+            g.DrawString($"{Node.Value}", new Font("Consolas", 30), Brushes.Black, left, top);
+        }
+    }
+
+    public class InternalNode
+    {
+        public int CountOnDirection(NodeLocation direction, TicTacToeValue value)
+        {
+            // starting from this direction count how many there are
+            var nextNode = Neighbours.FirstOrDefault(x => x.Location == direction && x.Node.Value == value);
+
+            if (nextNode == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + nextNode.Node.CountOnDirection(direction, value);
+            }
+        }
+
+        public TicTacToeValue Value;
+        private List<NeighbourLocation> Neighbours;
+        public InternalNode(TicTacToeValue value)
+        {
+            Value = value;
+            Neighbours = new List<NeighbourLocation>();
+        }
+
+        public void AddNeighbour(InternalNode node, NodeLocation location)
+        {
+            Neighbours.Add(new NeighbourLocation() { Location = location, Node = node });
+        }
+
+        public static InternalNode CreateX() => new InternalNode(TicTacToeValue.x);
+        public static InternalNode CreateO() => new InternalNode(TicTacToeValue.o);
+
+        internal void RemoveFromNeighbours()
+        {
+            // go through each of it's neighbours and remove this node from their list.
+            foreach (var node in Neighbours)
+            {
+                node.Node.RemoveNode(this);
+            }
+        }
+
+        internal void RemoveNode(InternalNode node)
+        {
+            var NodeAtLocation = Neighbours.FirstOrDefault(x => x.Node == node);
+            if (NodeAtLocation != null)
+            {
+                Neighbours.Remove(NodeAtLocation);
+            }
+        }
+
+        private class NeighbourLocation
+        {
+            public NodeLocation Location;
+            public InternalNode Node;
+        }
+
+    }
+
+    public enum NodeLocation
+    {
+        TopLeft = 2,
+        TopCenter = 3,
+        TopRight = 4,
+        Left = 5,
+        BottomRight = TopLeft + Constants.DirectionDifference,
+        BottomCenter = TopCenter + Constants.DirectionDifference,
+        BottomLeft = TopRight + Constants.DirectionDifference,
+        Right = Left + Constants.DirectionDifference
+    }
+
+    public enum TicTacToeValue
+    {
+        x = 0,
+        o = 1
+    }
+}
